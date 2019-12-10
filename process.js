b8ed2b7f17483899aca7588297f9d36fdadb0aa8
VictoryKS <victory.kosholap@gmail.com>
2019-12-10 20:57:36 +0200

    Optimized JS

--- process.js
+++ process.js
@@ -3,15 +3,20 @@
 const fs = require('fs');
 const path = require('path');
 
-function correctText(data) {
-  const str = data.toString();
+function correctText(str, prevEnd) {
   let res = '';
-  for (let i = 0; i < str.length; i++) {
+  if (prevEnd && prevEnd[1] === ',' && !str[0].match(/\s/) &&
+     (prevEnd[0].match(/[a-z]/i) || str[0].match(/[a-z]/i)))
+     res += " ";
+  for (let i = 0; i < str.length - 1 ; i++) {
     res += str[i];
-    if (str[i - 1] && str[i + 1] && str[i] === ','&& !str[i + 1].match(/\s/)
-        && (str[i - 1].match(/[a-z]/i) || str[i + 1].match(/[a-z]/i)))
+    if (str[i] === ',' && !str[i + 1].match(/\s/)
+        && (((str[i - 1] && str[i - 1].match(/[a-z]/i))
+        || (prevEnd && prevEnd[1].match(/[a-z]/i)))
+        || str[i + 1].match(/[a-z]/i)))
       res += " ";
   }
+  res += str[str.length - 1];
   return res;
 }
 
@@ -31,20 +36,30 @@ function correction(folderDir, resDir) {
   let files = fs.readdirSync(folderDir)
     .filter(fileName => fileName.endsWith('.txt'));
 
+  let counter = 0;
   for (let file of files) {
-    let result = '';
-    const stream = fs.createReadStream(folderDir + '/' + file)
-      .on('data', (data) => result += correctText(data))
-      .on('end', () => {
-        if (result) {
-          const resFile = resDir + '/' + file.substring(0, file.indexOf('.txt')) + '.res';
+    let prevDataEnd;
+    const stream = fs.createReadStream(folderDir + '/' + file, { highWaterMark: 64 })
+      .on('data', (data) => {
+        data = data.toString();
+        const result = correctText(data, prevDataEnd);
+        const resFile = resDir + '/' + file.substring(0, file.indexOf('.txt')) + '.res';
+        if (!prevDataEnd)
           fs.writeFile(resFile, result, (err) => {
             if (err) throw err;
-          })
-        }
+          });
+        else
+          fs.appendFile(resFile, result, (err) => {
+            if (err) throw err;
+          });
+        prevDataEnd = data.substring(data.length - 2, data.length);
+      })
+      .on('end', () => {
+        counter++;
+        if (counter == files.length)
+          console.log('Total number of processed files: ' + files.length);
       });
   }
-  console.log('Total number of processed files: ' + files.length);
 }
 
 // Usage
